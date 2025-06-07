a5b8077673155b337c39e509ba2f488cae800085
Rich Colvin <44912249+RichColvin@users.noreply.github.com>
2025-06-07 08:09:00 -0400

    Update Install.sh

--- Web Pages/SystemFiles/v1.0/Install.sh
+++ Web Pages/SystemFiles/v1.0/Install.sh
@@ -55,42 +55,58 @@
 #
 TITLE='\033[0;34;1;47m'		# Blue on Lt Gray
 KEYNOTE='\033[0;37;0;41m'	# Lt Gray on Red
-COMMENT='\033[0;30;0;47m'	# Black on Lt Gray
+CMNTTEXT='\033[0;34;1;40m'	# Blue on Black
 NOCOLOR='\e[0m'
 #
+echo -e "${TITLE}#######################################################################${NOCOLOR}"
+echo -e "${TITLE}#                                                                     #${NOCOLOR}"
+echo -e "${TITLE}#   RRRRRR     OOOOOO   SSSSSS  EEEEEEEE TTTTTTTT TTTTTTTT   AAAA     #${NOCOLOR}"
+echo -e "${TITLE}#   RR   RR   OO    OO SS    SS EE          TT       TT     AA  AA    #${NOCOLOR}"
+echo -e "${TITLE}#   RR    RR  OO    OO SS       EE          TT       TT    AA    AA   #${NOCOLOR}"
+echo -e "${TITLE}#   RR   RR   OO    OO  SS      EE          TT       TT    AA    AA   #${NOCOLOR}"
+echo -e "${TITLE}#   RRRRRR    OO    OO   SSSS   EEEEEE      TT       TT    AA    AA   #${NOCOLOR}"
+echo -e "${TITLE}#   RR   RR   OO    OO      SS  EE          TT       TT    AAAAAAAA   #${NOCOLOR}"
+echo -e "${TITLE}#   RR    RR  OO    OO       SS EE          TT       TT    AA    AA   #${NOCOLOR}"
+echo -e "${TITLE}#   RR    RR  OO    OO SS    SS EE          TT       TT    AA    AA   #${NOCOLOR}"
+echo -e "${TITLE}#   RR    RR   OOOOOO   SSSSSS  EEEEEEEE    TT       TT    AA    AA   #${NOCOLOR}"
+echo -e "${TITLE}#                                                                     #${NOCOLOR}"
+echo -e "${TITLE}#######################################################################${NOCOLOR}"
+echo -e "${TITLE}                                                                       ${NOCOLOR}"
+echo -e "${TITLE}Installation program                                                   ${NOCOLOR}"
+echo -e "${TITLE}                                                                       ${NOCOLOR}"
 # ********************************************************************
 # Step 1 - Setup the Mesa card.
 # 
-echo -e "${TITLE}##################################################${NOCOLOR}"
-echo -e "${TITLE}Step 1 - Mesa card${NOCOLOR}"
+echo -e "${TITLE}#######################################################################${NOCOLOR}"
+echo -e "${TITLE}Step 1 - Mesa card                                                     ${NOCOLOR}"
 #
 # Create the location where they are to be
-echo -e "${COMMENT}Creating location /usr/lib/firmware/hm2/hostmot2${NOCOLOR}"
+echo -e "${CMNTTEXT}Creating location /usr/lib/firmware/hm2/hostmot2${NOCOLOR}"
 cd /usr/lib/firmware/hm2
 sudo mkdir hostmot2
 #
 # Copy the files
-echo -e "${COMMENT}Copying the files to /usr/lib/firmware/hm2/hostmot2${NOCOLOR}"
+echo -e "${CMNTTEXT}Copying the files to /usr/lib/firmware/hm2/hostmot2${NOCOLOR}"
 cd /usr/lib/firmware/hm2/hostmot2
 sudo cp /home/cnc/Downloads/7i92t_rosetta.bin .
 sudo cp /home/cnc/Downloads/7i92t_rosetta.pin .
 sudo cp /home/cnc/Downloads/PIN_Rosetta_34.vhd .
 #
 # Flash the Mesa card
-echo -e "${COMMENT}Flashing the Mesa Card Configuration${NOCOLOR}"
+echo -e "${CMNTTEXT}Flashing the Mesa Card Configuration${NOCOLOR}"
 mesaflash --device 7i92t --addr 192.168.1.121 --write 7i92t_rosetta.bin --reload
 #
 # ********************************************************************
 # Step 2 - Copy Rosetta Files
-echo -e "${TITLE}##################################################${NOCOLOR}"
-echo -e "${TITLE}Step 2 - Rosetta configuration for LinuxCNC${NOCOLOR}"
+echo -e "${TITLE}#######################################################################${NOCOLOR}"
+echo -e "${TITLE}Step 2 - Rosetta configuration for LinuxCNC                            ${NOCOLOR}"
 #
 # Unzip the files to where they are to be
-echo -e "${COMMENT}Unzip the files to /home/cnc/lnuxcnc/configs/Rosetta${NOCOLOR}"
+echo -e "${CMNTTEXT}Unzip the files to /home/cnc/lnuxcnc/configs/Rosetta${NOCOLOR}"
 sudo unzip Rosetta.zip -d /home/cnc/lnuxcnc/configs 
 #
 # Set the security for the subdirectories
-echo -e "${COMMENT}All all access to /home/cnc/linuxcnc/configs/Rosetta/Rosetta_Axes${NOCOLOR}"
+echo -e "${CMNTTEXT}All all access to /home/cnc/linuxcnc/configs/Rosetta/Rosetta_Axes${NOCOLOR}"
 sudo chmod 777 -R /home/cnc/linuxcnc/configs/Rosetta/Rosetta_Axes
-echo -e "${COMMENT}All all access to /home/cnc/linuxcnc/configs/Rosetta/Rosetta_Display${NOCOLOR}"
+echo -e "${CMNTTEXT}All all access to /home/cnc/linuxcnc/configs/Rosetta/Rosetta_Display${NOCOLOR}"
 sudo chmod 777 -R /home/cnc/linuxcnc/configs/Rosetta/Rosetta_Display
